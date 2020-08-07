953868034670155b498dd5fbed7e8327807f2187
maxim <mmaximmax44@gmail.com>
2020-08-08 00:04:21 +0300

    Three basic modes done, i think.

--- src/main/java/here/lenrik1589/notreddust/config/NotRedConfig.java
+++ src/main/java/here/lenrik1589/notreddust/config/NotRedConfig.java
@@ -13,15 +13,68 @@ import me.shedaniel.clothconfig2.api.ConfigBuilder;
 import me.shedaniel.clothconfig2.api.ConfigCategory;
 import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
 import me.shedaniel.clothconfig2.impl.builders.DropdownMenuBuilder.TopCellElementBuilder;
+import me.shedaniel.clothconfig2.impl.builders.SubCategoryBuilder;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.block.Material;
+import net.minecraft.block.RedstoneWireBlock;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.nbt.*;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Identifier;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+
 public class NotRedConfig implements ModMenuApi {
 	private static final Logger LOGGER = LogManager.getLogger("notreddust.config");
+	private static final File configFile = new File(FabricLoader.getInstance().getConfigDirectory(),"notreddust.nbt");
+	public static final CompoundTag defaultConfig;
+	public static CompoundTag configTag;
+	static {
+		CompoundTag tag = new CompoundTag();
+		tag.putInt("Mode", ColorMode.OneColor.ordinal());
+		tag.putBoolean("Enabled", true);
+		{
+			CompoundTag Rainbow = new CompoundTag();
+			Rainbow.putBoolean("Folded", true);
+			Rainbow.putBoolean("Reversed", false);
+			Rainbow.putBoolean("PowerBrightness", false);
+			tag.put("Rainbow", Rainbow);
+		}{
+			CompoundTag OneColor = new CompoundTag();
+			OneColor.putBoolean("Folded", true);
+			OneColor.putIntArray("Color", new int[]{0, 255, 0, 0});
+			tag.put("OneColor", OneColor);
+		}{
+			CompoundTag Gradient = new CompoundTag();
+			Gradient.putInt("Mode", LerpMode.RGB.ordinal());
+			Gradient.putBoolean("Folded", true);
+			Gradient.putIntArray("Start", new int[]{255, 255, 0, 0});
+			Gradient.putIntArray("End", new int[]{255, 0, 255, 0});
+			tag.put("Gradient", Gradient);
+		}
+		defaultConfig = tag;
+		try {
+			configTag = NbtIo.read(configFile);
+		} catch (IOException e) {
+			configTag = new CompoundTag().copyFrom(defaultConfig);
+			try {
+				LOGGER.error("Error occurred while reading config file, falling back to default config:", e);
+				NbtIo.write(configTag, configFile);
+			} catch (IOException ioException) {
+				LOGGER.error("Error occurred while writing default config to file config file:", e);
+			}
+		}
+		if (configTag == null) {
+			configTag = defaultConfig;
+		}
+	}
+
+
 	public NotRedConfig () {
 	}
 
@@ -30,47 +83,246 @@ public class NotRedConfig implements ModMenuApi {
 	}
 
 	public ConfigScreenFactory<?> getModConfigScreenFactory() {
-		return (screen) -> defaultConfigBuilder(screen).build();
+		return (screen) -> defaultConfigBuilder().build();
 	}
 
-	enum Mode{
+	public enum ColorMode implements Serializable {
 		Rainbow,
-		OneColor
+		OneColor,
+		Gradient;
+		public static ColorMode load (){
+			int value = loadInt("Mode");
+			return values()[value];
+		}
+		public static void save(ColorMode value){
+			saveInt(value.ordinal(), "Mode");
+		}
 	}
 
-	public static ConfigBuilder defaultConfigBuilder (Screen screen) {
+	public enum LerpMode implements Serializable {
+		RGB,
+		HSB;
+		public static LerpMode load (){
+			int value = loadInt("Mode", "Gradient");
+			return values()[value];
+		}
+		public static void save(LerpMode value){
+			saveInt(value.ordinal(), "Mode", "Gradient");
+		}
+	}
+
+	public static ConfigBuilder defaultConfigBuilder () {
 //		LOGGER.info(screen.width + "×" + screen.height);
-		ConfigBuilder builder = ConfigBuilder.create().setParentScreen(MinecraftClient.getInstance().currentScreen).setTitle(new TranslatableText("title.notreddust.config"));
+		ConfigBuilder builder = ConfigBuilder.create()
+						.setParentScreen(MinecraftClient.getInstance().currentScreen)
+						.setTitle(new TranslatableText("notreddust.config.title"));
 		builder.setDefaultBackgroundTexture(new Identifier("minecraft:textures/block/redstone_block.png"));
 		builder.setGlobalized(true);
-		ConfigEntryBuilder entryBuilder = builder.entryBuilder();
-
-		ConfigCategory testing = builder.getOrCreateCategory(new TranslatableText("category.notreddust.main"));
-		testing
-						.addEntry(
-										entryBuilder
-														.startDropdownMenu(
-																		new TranslatableText("notreddust.mode"),
-																		TopCellElementBuilder.of(
-																						Mode.Rainbow,
-																						(s) -> {
-																							try {
-																								return Mode.valueOf(s);
-																							} catch (NumberFormatException var2) {
-																								return null;
-																							}
-																						}
-																						)
-														)
-														.setDefaultValue(Mode.Rainbow)
-														.setSuggestionMode(false)
-														.setSelections(Lists.newArrayList(Mode.Rainbow,Mode.OneColor))
-														.setSaveConsumer(LOGGER::info)
-														.setTooltip(new TranslatableText("notreddust.mode.desc"))
-														.build()
-						);//AFK
-		testing.setCategoryBackground(new Identifier("minecraft","textures/block/red_stained_glass.png"));
 		builder.transparentBackground();
+		ConfigEntryBuilder entryBuilder = builder.entryBuilder();
+		ConfigCategory testing = builder.getOrCreateCategory(new TranslatableText("notreddust.category.main"));
+		ColorMode currentColorMode = ColorMode.load();
+		testing.addEntry(
+						entryBuilder.startBooleanToggle(new TranslatableText("notreddust.enabled"), loadBoolean("Enabled"))
+										.setDefaultValue(true).setSaveConsumer(bool -> saveBoolean(bool, "Enabled"))
+										.setTooltip(new TranslatableText("notreddust.enabled.desc"))
+										.build());
+		testing.addEntry(
+						entryBuilder
+										.startEnumSelector(
+														new TranslatableText("notreddust.mode"),
+														ColorMode.class,
+														ColorMode.load()
+										)
+										.setDefaultValue(ColorMode.OneColor)
+										.setSaveConsumer(ColorMode::save)
+										.setTooltip(new TranslatableText("notreddust.mode.desc"))
+										.build()
+		);
+		SubCategoryBuilder modeSettingsCategory = entryBuilder
+						.startSubCategory(new TranslatableText("notreddust.category.mode"))
+						.setTooltip(new TranslatableText("notreddust.category.mode.desc"));
+		if(currentColorMode == ColorMode.Rainbow){
+			modeSettingsCategory.add(
+							entryBuilder
+											.startBooleanToggle(
+															new TranslatableText("notreddust.rainbow.reverse"),
+															loadBoolean("Reversed","Rainbow")
+											)
+											.setDefaultValue(false).setTooltip(new TranslatableText("notreddust.rainbow.reverse.desc"))
+											.setSaveConsumer(bool -> saveBoolean(bool, "Reversed", "Rainbow")).build());
+			modeSettingsCategory.add(
+							entryBuilder
+											.startBooleanToggle(
+															new TranslatableText("notreddust.rainbow.power_brightness"),
+															loadBoolean("PowerBrightness", "Rainbow")
+											)
+											.setDefaultValue(false).setTooltip(new TranslatableText("notreddust.rainbow.power_brightness.desc"))
+											.setSaveConsumer(bool -> saveBoolean(bool, "PowerBrightness", "Rainbow")).build());
+		} else if (currentColorMode == ColorMode.OneColor){
+			modeSettingsCategory.add(
+							entryBuilder
+											.startColorField(
+															new TranslatableText("notreddust.one.color"),
+															loadColor("Color", "OneColor")
+											)
+											.setDefaultValue(0xff0000).setTooltip(new TranslatableText("notreddust.one.color.desc"))
+											.setSaveConsumer(color -> saveColor(color, "Color","OneColor")).build());
+		} else {
+			modeSettingsCategory.add(
+							entryBuilder
+											.startEnumSelector(
+															new TranslatableText("notreddust.gradient.mode"),
+															LerpMode.class,
+															LerpMode.load()
+											)
+											.setDefaultValue(LerpMode.RGB)
+											.setSaveConsumer(LerpMode::save)
+											.setTooltip(new TranslatableText("notreddust.gradient.mode.desc"))
+											.build()
+			);
+			modeSettingsCategory.add(
+							entryBuilder
+											.startColorField(
+															new TranslatableText("notreddust.gradient.start"),
+															loadColor("Start", "Gradient")
+											)
+											.setDefaultValue(0xff0000).setTooltip(new TranslatableText("notreddust.gradient.start.desc"))
+											.setSaveConsumer(color -> saveColor(color, "Start","Gradient")).build());
+			modeSettingsCategory.add(
+							entryBuilder
+											.startColorField(
+															new TranslatableText("notreddust.gradient.end"),
+															loadColor("End", "Gradient")
+											)
+											.setDefaultValue(0x00ff00).setTooltip(new TranslatableText("notreddust.gradient.end.desc"))
+											.setSaveConsumer(color -> saveColor(color, "End","Gradient")).build());
+		}
+		testing.addEntry(modeSettingsCategory.build());
+		builder.setSavingRunnable(NotRedConfig::save);
 		return builder;
+	}
+
+	// Whole save ——————————————————————————
+
+	public static void save(){
+		LOGGER.info("Saving settings.");
+		try {
+			NbtIo.write(configTag, configFile);
+		} catch (IOException e) {
+			LOGGER.error("Error occurred while saving config:", e);
+		} finally {
+			new RedstoneWireBlock(FabricBlockSettings.of(Material.METAL).hardness(4.0f));
+		}
+	}
+
+	// boolean stuff ———————————————————————
+	public static boolean loadBoolean(String name, String... groups){
+		try {
+			CompoundTag currentTag = configTag;
+			for (String group : groups){
+				currentTag = currentTag.getCompound(group);
+			}
+			return currentTag.getBoolean(name);
+		} catch (NullPointerException e){
+			LOGGER.error("Empty config or key ${name} is not present:", e);
+			CompoundTag currentTag = defaultConfig;
+			for (String group : groups){
+				currentTag = currentTag.getCompound(group);
+			}
+			return currentTag.getBoolean(name);
+		}
+	}
+
+	public static void saveBoolean(boolean state, String name, String... groups){
+		CompoundTag currentTag = configTag;
+		for (String group : groups){
+			currentTag = currentTag.getCompound(group);
+		}
+		currentTag.putBoolean(name, state);
+		if(groups.length > 0) {
+			for (int i = groups.length - 1; i >= 0; i--) {
+				CompoundTag parentTag = configTag;
+				int j;
+				for (j = 0; j < i; j++) {
+					parentTag = parentTag.getCompound(groups[j]);
+				}
+				parentTag.put(groups[j], currentTag);
+			}
+		}
+	}
+
+	// int stuff ———————————————————————
+	public static int loadInt(String name, String... groups){
+		try {
+			CompoundTag currentTag = configTag;
+			for (String group : groups){
+				currentTag = currentTag.getCompound(group);
+			}
+			return currentTag.getInt(name);
+		} catch (NullPointerException e){
+			LOGGER.error("Empty config or key ${name} is not present:", e);
+			CompoundTag currentTag = defaultConfig;
+			for (String group : groups){
+				currentTag = currentTag.getCompound(group);
+			}
+			return currentTag.getInt(name);
+		}
+	}
+
+	public static void saveInt(int state, String name, String... groups){
+		CompoundTag currentTag = configTag;
+		for (String group : groups){
+			currentTag = currentTag.getCompound(group);
+		}
+		currentTag.putInt(name, state);
+		if(groups.length > 0) {
+			for (int i = groups.length - 1; i >= 0; i--) {
+				CompoundTag parentTag = configTag;
+				int j;
+				for (j = 0; j < i; j++) {
+					parentTag = parentTag.getCompound(groups[j]);
+				}
+				parentTag.put(groups[j], currentTag);
+			}
+		}
+	}
+
+	// Color Stuff —————————————————————————
+
+	public static int loadColor(String name, String... groups){
+		try {
+			CompoundTag currentTag = configTag;
+			for (String group : groups){
+				currentTag = currentTag.getCompound(group);
+			}
+			int[] arr = currentTag.getIntArray(name).length == 0 ? new int[4] : currentTag.getIntArray(name);
+			return ((arr[0]) << 24) + ((arr[1]) << 16) + ((arr[2]) << 8) + arr[3];
+		} catch (NullPointerException e) {
+			LOGGER.error("Empty config or key ${name} is not present:", e);
+			CompoundTag currentTag = defaultConfig;
+			for (String group : groups){
+				currentTag = currentTag.getCompound(group);
+			}
+			int[] arr = currentTag.getIntArray(name).length == 0 ? new int[4] : currentTag.getIntArray(name);
+			return ((arr[0]) << 24) + ((arr[1]) << 16) + ((arr[2]) << 8) + arr[3];
+		}
+	}
+	public static void saveColor(int color, String name, String... groups){
+		CompoundTag currentTag = configTag;
+		for (String group : groups){
+			currentTag = currentTag.getCompound(group);
+		}
+		currentTag.putIntArray(name, new int[]{(color >> 24) & 0xff, (color >> 16) & 0xff, (color >> 8) & 0xff, color & 0xff});
+		if(groups.length > 0) {
+			for (int i = groups.length - 1; i >= 0; i--) {
+				CompoundTag parentTag = configTag;
+				int j;
+				for (j = 0; j < i; j++) {
+					parentTag = parentTag.getCompound(groups[j]);
+				}
+				parentTag.put(groups[j], currentTag);
+			}
+		}
 	}
 }
--- src/main/java/here/lenrik1589/notreddust/mixin/RedstoneWireBlockMixin.java
+++ src/main/java/here/lenrik1589/notreddust/mixin/RedstoneWireBlockMixin.java
@@ -1,8 +1,10 @@
 package here.lenrik1589.notreddust.mixin;
 
+import here.lenrik1589.notreddust.config.NotRedConfig;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.RedstoneWireBlock;
 import net.minecraft.client.util.math.Vector3f;
+import net.minecraft.util.math.MathHelper;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.spongepowered.asm.mixin.Final;
@@ -11,6 +13,7 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
 import java.awt.Color;
 
 @Mixin(RedstoneWireBlock.class)
@@ -18,16 +21,82 @@ public class RedstoneWireBlockMixin {
 
 	private static final Logger LOGGER = LogManager.getLogger("notreddust");
 
-	@Final
-	@Shadow
+	@Final @Shadow
 	private static Vector3f[] field_24466;
 
 	@Inject(at = @At("TAIL"), method = "<init>(Lnet/minecraft/block/AbstractBlock$Settings;)V")
 	private void init(AbstractBlock.Settings settings, CallbackInfo info) {
-		for(int i = 0; i < 16; i++) {
-			int color = Color.HSBtoRGB(i / 16f, 1, 1);
-			LOGGER.info(((color >> 16) & 0xff) + " " + ((color >> 8) & 0xff) + " " + ((color) & 0xff));
-			field_24466[i] = new Vector3f(((color >> 16) & 0xff) / 256f, ((color >> 8) & 0xff) / 256f, ((color) & 0xff) / 256f);
+		if(NotRedConfig.loadBoolean("Enabled")){
+			NotRedConfig.ColorMode colorMode = NotRedConfig.ColorMode.load();
+			LOGGER.info("Applying color in {} mode.",colorMode.toString());
+			if(colorMode == NotRedConfig.ColorMode.Rainbow){
+				boolean reversed = NotRedConfig.loadBoolean("Reversed", "Rainbow");
+				boolean powerBrightness = NotRedConfig.loadBoolean("PowerBrightness", "Rainbow");
+				LOGGER.info(" " +reversed);
+				for(int powerLevel = 0; powerLevel < 16; powerLevel++) {
+					int color = Color.HSBtoRGB(
+									reversed? 1 - powerLevel / 16f : powerLevel / 16f,
+									1,
+									powerBrightness?(0.55f + (powerLevel + 1) / 16f * 0.45f):1);
+					LOGGER.info("{}: {}r {}g {}b", powerLevel, (color >> 16) & 0xff, (color >> 8) & 0xff, color & 0xff);
+					field_24466[powerLevel] = new Vector3f(((color >> 16) & 0xff) / 256f, ((color >> 8) & 0xff) / 256f, ((color) & 0xff) / 256f);
+				}
+			} else if (colorMode == NotRedConfig.ColorMode.OneColor){
+				int color = NotRedConfig.loadColor("Color","OneColor");
+				float[] hsbColor = Color.RGBtoHSB((color >> 16) & 0xff, (color >> 8) & 0xff, (color) & 0xff, new float[3]);
+				for(int powerLevel = 0; powerLevel < 16; powerLevel++) {
+					color = Color.HSBtoRGB(hsbColor[0], hsbColor[1], (hsbColor[2] *(0.55f + powerLevel / 16f * 0.45f)));
+					LOGGER.info("{}: {}r {}g {}b", powerLevel, (color >> 16) & 0xff, (color >> 8) & 0xff, color & 0xff);
+					field_24466[powerLevel] = new Vector3f(((color >> 16) & 0xff) / 256f, ((color >> 8) & 0xff) / 256f, ((color) & 0xff) / 256f);
+				}
+			} else {
+				NotRedConfig.LerpMode lerpMode = NotRedConfig.LerpMode.load();
+				LOGGER.info("Lerping color using {} values",lerpMode);
+				int color1 = NotRedConfig.loadColor("Start", "Gradient");
+				int color2 = NotRedConfig.loadColor("End", "Gradient");
+				float[] hsbColor1 = Color.RGBtoHSB((color1 >> 16) & 0xff, (color1 >> 8) & 0xff, color1 & 0xff, new float[3]);
+				float[] hsbColor2 = Color.RGBtoHSB((color2 >> 16) & 0xff, (color2 >> 8) & 0xff, color2 & 0xff, new float[3]);
+				for (int powerLevel = 0; powerLevel < 16; powerLevel++) {
+					int color;
+					if(lerpMode == NotRedConfig.LerpMode.RGB) {
+						color = lerpColorAndAlpha(powerLevel / 16d, color1, color2);
+					} else {
+						float lerpedHue        = lerp(powerLevel / 15d, hsbColor1[0], hsbColor2[0]);
+						float lerpedSaturation = lerp(powerLevel / 15d, hsbColor1[1], hsbColor2[1]);
+						float lerpedBrightness = lerp(powerLevel / 15d, hsbColor1[2], hsbColor2[2]);
+						LOGGER.info("{}: {}h {}s {}b", powerLevel, lerpedHue, lerpedSaturation, lerpedBrightness);
+						color = Color.HSBtoRGB(lerpedHue, lerpedSaturation, lerpedBrightness);
+					}
+					LOGGER.info("{}: {}r {}g {}b", powerLevel, (color >> 16) & 0xff, (color >> 8) & 0xff, color & 0xff);
+					field_24466[powerLevel] = new Vector3f(((color >> 16) & 0xff) / 256f, ((color >> 8) & 0xff) / 256f, ((color) & 0xff) / 256f);
+				}
+			}
+		} else {
+			LOGGER.info("Applying vanilla coloring.");
+			for(int powerLevel = 0; powerLevel <= 15; ++powerLevel) {
+				float value = powerLevel / 15.0F;
+				float R = value * 0.6F + (value > 0.0F ? 0.4F : 0.3F);
+				float G = MathHelper.clamp(value * value * 0.7F - 0.5F, 0.0F, 1.0F);
+				float B = MathHelper.clamp(value * value * 0.6F - 0.7F, 0.0F, 1.0F);
+				LOGGER.info("{}: {}r {}g {}b", powerLevel, R * 0xff, G * 0xff, B * 0xff);
+				field_24466[powerLevel] = new Vector3f(R, G, B);
+			}
 		}
+	}
+
+	private int lerp(double delta, int to, int from) {
+		return (int)(delta * (double)to + (1.0D - delta) * (double)from);
+	}
+
+	private float lerp(double delta, float to, float from) {
+		return (float) (delta * (double)to + (1.0D - delta) * (double)from);
+	}
+
+	private int lerpColorAndAlpha(double delta, int to, int from){
+		return
+						((lerp(delta, (to >> 24) & 0xFF, (from >> 24) & 0xFF) << 24) & 0xFF000000) |
+						((lerp(delta, (to >> 16) & 0xFF, (from >> 16) & 0xFF) << 16) & 0x00FF0000) |
+						((lerp(delta, (to >> 8)  & 0xFF, (from >> 8)  & 0xFF) << 8 ) & 0x0000FF00) |
+						((lerp(delta, to         & 0xFF, from         & 0xFF)      ) & 0x000000FF);
 	}
 }
